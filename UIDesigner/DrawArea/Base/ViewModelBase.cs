2dac1350472170e2a21aeba1b6386ec316a36d5e
RishabhKatiyar <rishabh.katiyar@outlook.com>
2016-08-30 23:26:02 +0530

    Arranging Code Part - 2

---
+++ UIDesigner/DrawArea/Base/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace DrawArea
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ UIDesigner/DrawArea/InputBlockProperties/InputBlockPropertiesViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DrawArea
+{
+    public class InputBlockPropertiesViewModel : ViewModelBase
+    {
+        string id;
+        public string ID
+        {
+            get
+            { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    RaisePropertyChangedEvent("ID");
+                }
+            }
+        }
+
+        string row;
+        public string Row
+        {
+            get
+            { return row; }
+            set
+            {
+                if (row != value)
+                {
+                    row = value;
+                    RaisePropertyChangedEvent("Row");
+                }
+            }
+        }
+
+        string col;
+        public string Col
+        {
+            get
+            { return col; }
+            set
+            {
+                if (col != value)
+                {
+                    col = value;
+                    RaisePropertyChangedEvent("Col");
+                }
+            }
+        }
+
+        string _dmlKeyword;
+        public string dmlKeyword
+        {
+            get
+            { return _dmlKeyword; }
+            set
+            {
+                if (_dmlKeyword != value)
+                {
+                    _dmlKeyword = value;
+                    RaisePropertyChangedEvent("dmlKeyword");
+                }
+            }
+        }
+
+        Visibility _inputBlockVisibility;
+        public Visibility inputBlockVisibility
+        {
+            get
+            {
+                return _inputBlockVisibility;
+            }
+            set
+            {
+                if (_inputBlockVisibility != value)
+                {
+                    _inputBlockVisibility = value;
+                    RaisePropertyChangedEvent("inputBlockVisibility");
+                }
+            }
+        }
+
+    }
+}
